0d380db915a5148610a0069f19b25229f73d1f4f
Daiana Koblitz <69736274+ILadyLuckI@users.noreply.github.com>
2023-03-22 17:11:42 -0300

    Arquivo adicionado via upload

---
+++ 5-atribuicaoDeVariaveis.js
@@ -0,0 +1,15 @@
+console.log("Trabalhando com atribuição de variáveis");
+
+const idade = 36;
+const primeiroNome = "Daiana";
+const sobrenome = "Koblitz";
+
+console.log(primeiroNome + sobrenome);
+console.log(primeiroNome, sobrenome);
+console.log(primeiroNome + " " + sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+
+nomeCompleto = 2;
